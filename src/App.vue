450e25a6908f386731de0b1b19165f53a25adc4d
Julio <almiror.info@gmail.com>
2023-12-14 18:45:58 -0500

    INICIO DEL PROYECTO

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ContadorsDeNumero from '@/components/ContadorDeNumero.vue'
+</script>
+
+<template>
+  <ContadorsDeNumero/>
+</template>
---
+++ src/components/ContadorDeNumero.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+let numero = ref(0);
+</script>
+
+<template>
+  <div>
+    <button @click="numero++">Aumentar</button>
+    <button @click="numero--">Disminuir</button>
+    <div>
+      {{ numero }}
+    </div>
+  </div>
+</template>
